c2ae6907b95cd47b3d64ccba147d07faf313d349
Dipayan Das <dipayan.das2010@gmail.com>
2020-06-12 21:21:06 +0530

    LSM paramters

--- LSM_init.py
+++ LSM_init.py
@@ -2,4 +2,7 @@ import numpy as np
 
 class LSM_init:
     def __init__(self, input_size, width, height, output_size):
-        pass
+        self.input_size = input_size
+        self.width = width
+        self.height = height
+        self.output_size = output_size
