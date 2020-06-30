1d3b8174d67ff1f239d437e196a888868c81de53
Dipayan Das <dipayan.das2010@gmail.com>
2020-06-30 23:10:28 +0530

    LSM updated : Added Short term plasticity

--- Project I/LSM.py
+++ Project I/LSM.py
@@ -4,7 +4,8 @@ from neuron_models import LIF
 
 class LSM:
     def __init__(self, input_size, width, height, output_size, 
-                 lamda=5, primary_to_auxiliary_ratio=0.6, input_feature_selection_sparsity=0.6):
+                 lamda=5, primary_to_auxiliary_ratio=0.6, input_feature_selection_sparsity=0.6, neuron_type_ratio=0.8,
+                 stp_alpha = 0.01, stp_beta = 0.3):
         self.input_size = input_size
         self.width = width
         self.height = height
@@ -13,6 +14,9 @@ class LSM:
         self.num_of_neurons = self.width * self.height
         self.liquid_layer_neurons = [LIF(tau_m=1) for _ in range(self.num_of_neurons)]
         
+        self.stp_alpha = stp_alpha
+        self.stp_beta = stp_beta
+
         #create dictioanries : neurons <=> coordinates
         self.neuron_to_coordinate = {}
         for i in range(self.height):
@@ -25,7 +29,7 @@ class LSM:
 
         #create dictionary : neuron -> excitory ('E') / Inhibitory ('I')
         self.get_neuron_type = {}
-        self.neuron_type_ratio = 0.8 #60% Excitory & 40% Inhibitory
+        self.neuron_type_ratio = neuron_type_ratio #80% Excitory & 20% Inhibitory
         excitory_neuron_indices = np.random.randint(0, self.num_of_neurons, int(self.neuron_type_ratio*self.num_of_neurons))
         for n in range(self.num_of_neurons):
             if n in excitory_neuron_indices:
@@ -36,7 +40,7 @@ class LSM:
         #create input and output weight matrices (considering only excitory synapses)
         self.primary_to_auxiliary_ratio = primary_to_auxiliary_ratio #default: 60% of the liquid layer neurons receive inputs
         self.input_feature_selection_sparsity = input_feature_selection_sparsity #default:40% of input vector will be alotted to a primary neuron
-        self.input_weight_matrix = np.zeros((self.num_of_neurons, self.input_size))        	
+        self.input_weight_matrix = np.zeros((self.num_of_neurons, self.input_size))         
         self.primary_neuron_indices = np.random.randint(0, self.num_of_neurons, int(self.primary_to_auxiliary_ratio*self.num_of_neurons))
         for n in self.primary_neuron_indices:
             input_vector_indices = np.random.randint(0, self.input_size, int((1-self.input_feature_selection_sparsity)*self.input_size))
@@ -50,6 +54,7 @@ class LSM:
         self.C = {'EE': 0.6,'EI': 1,'II': 0.2,'IE': 1}
         self.lamda = {'EE': lamda,'EI': lamda,'II': lamda,'IE': lamda}
         self.synaptic_strength = {'EE': 3,'EI': 3,'II': -1,'IE': -4}    #Inhibitory synapses are given negetive strengths
+        
         probability_of_connection = []
         for n_1 in self.liquid_layer_neurons:
             temp = []
@@ -61,13 +66,15 @@ class LSM:
             probability_of_connection.append(temp)
         #normalize probability of connections
         probability_of_connection /= np.max(probability_of_connection)
+        #Following the calculated probabilty, allocate weights
         for i,n_1 in enumerate(self.liquid_layer_neurons):
             for j,n_2 in enumerate(self.liquid_layer_neurons):
                 rand = np.random.uniform(0.000, 1.000, 1)
                 if rand < probability_of_connection[i][j]:
                     connection_type = self.get_neuron_type[n_1]+self.get_neuron_type[n_2]
                     self.liquid_weight_matrix[i][j] = self.synaptic_strength[connection_type]
-        #Normalize sum of excitory and sum of inhibitory synapses for each neuron in liquid layer
+
+        #Normalize (separately) sum of excitory and sum of inhibitory pre-synapses for each neuron in liquid layer
         for c in range(self.liquid_weight_matrix.shape[1]):
             pre_synapses = self.liquid_weight_matrix[:,c]
             total_excitory_syn_strength, total_inhibitory_syn_strength = 0, 0
@@ -77,14 +84,16 @@ class LSM:
                 else:
                     total_inhibitory_syn_strength +=s
             for s_idx in range(pre_synapses.shape[0]):
-                if pre_synapses[s_idx]>=0:
-                    pre_synapses[s_idx] /= total_excitory_syn_strength
-                else:
-                    pre_synapses[s_idx] /= -total_inhibitory_syn_strength
-            self.liquid_weight_matrix[:,c] = pre_synapses[:]
+                try:
+                    if pre_synapses[s_idx]>=0:
+                        pre_synapses[s_idx] /= total_excitory_syn_strength
+                    else:
+                        pre_synapses[s_idx] /= -total_inhibitory_syn_strength
+                except:
+                    print(total_excitory_syn_strength, total_inhibitory_syn_strength)
 
-        print(self.liquid_weight_matrix)
-        
+            self.liquid_weight_matrix[:,c] = pre_synapses[:]      
+
         '''
         plt.figure(figsize=(10,10), dpi=200)
         plt.imshow(self.liquid_weight_matrix, cmap='gray')
@@ -95,15 +104,18 @@ class LSM:
     def get_activation(self, ST_input, simulation_time=10000):
         
         N_t = np.zeros((len(self.liquid_layer_neurons),)) #state vector
+        S_t = np.ones((len(self.liquid_layer_neurons),)) #STP vector
         activation = [] #activation of LSM over entire simulation time
 
         for t in range(simulation_time):
+            
             input_current = np.dot(self.input_weight_matrix, ST_input[:,t])
-            past_current = np.dot(self.liquid_weight_matrix.T, N_t)
+            past_current = np.dot(self.liquid_weight_matrix.T, N_t) 
             total_current = input_current + past_current
+            
             temp_activation = []
-
             for idx, neuron in enumerate(self.liquid_layer_neurons):
+                
                 new_Vmem = neuron.update(total_current[idx], t)
                 
                 if new_Vmem == neuron.V_spike:
@@ -111,9 +123,10 @@ class LSM:
                 else:
                     temp_activation.append(0)
 
-            N_t = temp_activation
+            N_t = np.asarray(temp_activation) * S_t              #Modulate output amplitude using STP protocol
+            S_t = S_t - (self.stp_alpha*(N_t - self.stp_beta))
 
-            activation.append(np.asarray(temp_activation))
+            activation.append(N_t)
 
         return np.asarray(activation)
 
--- Project I/script_test.py
+++ Project I/script_test.py
@@ -1,4 +1,4 @@
-#from LSM import LSM
+from LSM import LSM
 from neuron_models import LIF
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,25 +22,29 @@ for t in range(10000):  #ms
 plt.plot(activation)
 plt.plot([x/100 for x in spikes])
 plt.show()
-
+'''
 
 #LSM simulation----------------------------------------------------------------------------
 sim_time = 500
 input_dim = 20
-w, h = 3,3
+w, h = 6, 6
 
-lsm = LSM(input_size = input_dim, lamda = 20, output_size=5, width=w, height=h)
+lsm = LSM(input_size = input_dim, lamda = 30, output_size=5, width=w, height=h, stp_alpha=0.01, stp_beta=0.3)
 
 #x = np.ones((input_dim, int(sim_time/2)))*20
 #y = np.zeros((input_dim, int(sim_time/2)))
 
-x = np.random.randint(0,2,size=(input_dim,int(sim_time/2)))*20
-y = np.random.randint(0,2,size=(input_dim,int(sim_time/2)))*40
+#x = np.random.randint(0,2,size=(input_dim,int(sim_time/2)))*20
+#y = np.random.randint(0,2,size=(input_dim,int(sim_time/2)))*40
+
+x = np.random.normal(loc = 5, scale = 1, size=(input_dim,int(sim_time/2))) * 4
+y = np.random.uniform(0, 1, size=(input_dim,int(sim_time/2))) * 20
+
 ST_input = np.hstack((x,y))
 
 act = lsm.get_activation(ST_input=ST_input, simulation_time=sim_time)
-
-for t,s in enumerate(states):
+'''
+for t,s in enumerate(act):
     try:
         s = s.reshape(w,h)
         plt.imshow(s)
@@ -49,14 +53,15 @@ for t,s in enumerate(states):
     except:
         break
 plt.show()
-'''
+
 '''
 print(act.shape)
 plt.imshow(act.T)
 plt.show()
-'''
+
 
 #Environment Simulation------------------------------------------------------------------
+'''
 import pyautogui as gui
 
 num_objects = 10
@@ -70,3 +75,4 @@ while gui.position() != (0,0):
     r,f  = np.interp(x,[0,1000],[-0.5, 0.5]), np.interp(y,[0,700],[5, 0])
 
     R_o, sin_o, cos_o, R_f, sin_f, cos_f, sin_h, cos_h, health = env.step(forward=f, rotate=r)
+'''
